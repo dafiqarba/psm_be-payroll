eefa78f303a10732e664cab667ddf6a92749db6e
dafiqarba <daviqarba@live.com>
2022-01-26 20:41:50 +0700

    Only allows ASC or DESC on /leave-record-list endpoint parameter

--- services/leave-record-service.go
+++ services/leave-record-service.go
@@ -23,10 +23,14 @@ func NewLeaveRecordService(leaveRecordRepo repository.LeaveRecordRepo) LeaveReco
 	}
 }
 
-func (service *leaveRecordService) GetLeaveRecordDetail(req_id int, id int) (entity.LeaveRecord,error) {
+func (service *leaveRecordService) GetLeaveRecordDetail(req_id int, id int) (entity.LeaveRecord, error) {
 	return service.leaveRecordRepository.GetLeaveRecordDetail(req_id, id)
 }
 
-func (service *leaveRecordService) GetLeaveRecordList(id int, year string) ([]entity.LeaveRecordListModel,error) {
+func (service *leaveRecordService) GetLeaveRecordList(id int, year string) ([]entity.LeaveRecordListModel, error) {
+	if year == "ASC" || year == "DESC" {
+		return service.leaveRecordRepository.GetLeaveRecordList(id, year)
+	}
+	year = "ASC"
 	return service.leaveRecordRepository.GetLeaveRecordList(id, year)
 }
