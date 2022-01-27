edd7e971149fcf0a982420c34ddbc7cdc236e036
dafiqarba <daviqarba@live.com>
2022-01-27 18:06:49 +0700

    Add leave record dto model

---
+++ dto/leave-record-dto.go
@@ -0,0 +1,15 @@
+package dto
+
+// Model response when user is creating a leave request
+type CreateLeaveRecordModel struct {
+	Request_on  string `json:"request_on"`
+	From_date   string `json:"from_date"`
+	To_date     string `json:"to_date"`
+	Return_date string `json:"return_date"`
+	Reason      string `json:"reason"`
+	Mobile      string `json:"mobile"`
+	Address     string `json:"address"`
+	Status_id   string `json:"status_id"`
+	Leave_id    string `json:"leave_id"`
+	User_id     string `json:"user_id"`
+}
