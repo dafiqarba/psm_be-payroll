405557d795b56cbd0d6c840aa21d3cc3ce1fccf5
dafiqarba <daviqarba@live.com>
2022-01-27 18:00:52 +0700

    Modify success response builder struct

--- utils/success-response.go
+++ utils/success-response.go
@@ -2,17 +2,31 @@ package utils
 
 //JSON success response model
 type SuccessResponse struct {
-	Status  int         `json:"status"`
+	Status  int         `json:"status_code"`
 	Message string      `json:"message"`
 	Data    interface{} `json:"data"`
+}
+type InsertSuccessResponse struct {
+	Status     int    `json:"status_code"`
+	Message    string `json:"message"`
+	Request_id int    `json:"request_id"`
 }
 
 //Success response builder
 func ResponseJSON(status int, message string, data interface{}) SuccessResponse {
 	result := SuccessResponse{
 		Status:  status,
-		Message: message,
+		Message: "success",
 		Data:    data,
+	}
+	return result
+}
+
+func InsertResponseJSON(status int, message string, data int) InsertSuccessResponse {
+	result := InsertSuccessResponse{
+		Status:     status,
+		Message:    message,
+		Request_id: data,
 	}
 	return result
 }
