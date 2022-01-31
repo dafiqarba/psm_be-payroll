e57db6f95289c1f71a44cdea5a3fcc9edb4e9943
dafiqarba <daviqarba@live.com>
2022-01-31 18:03:35 +0700

    Add login and register route

--- main.go
+++ main.go
@@ -9,6 +9,7 @@ import (
 	"github.com/dafiqarba/be-payroll/controller"
 	"github.com/dafiqarba/be-payroll/repository"
 	"github.com/dafiqarba/be-payroll/services"
+	"github.com/gorilla/handlers"
 	"github.com/gorilla/mux"
 )
 
@@ -26,10 +27,21 @@ var (
 	leaveRecordRepository repository.LeaveRecordRepo       = repository.NewLeaveRecordRepo(db)
 	leaveRecordService    services.LeaveRecordService      = services.NewLeaveRecordService(leaveRecordRepository)
 	leaveRecordController controller.LeaveRecordController = controller.NewLeaveRecordController(leaveRecordService)
+
+	authService services.AuthService = services.NewAuthService(userRepository)
+	authController controller.AuthController = controller.NewAuthController(authService, userService)
 )
 
 func main() {
 	router := mux.NewRouter()
+
+    a := handlers.AllowedHeaders([]string{"content-type"})
+    // handlers.AllowedHeaders([]string{"Access-Control-Allow-Origin"}),
+    b := handlers.AllowedHeaders([]string{"X-Requested-With"})
+    c := handlers.AllowedOrigins([]string{"*"})
+	d := handlers.AllowedMethods([]string{"GET", "HEAD", "POST", "PUT", "OPTIONS"})
+    e := handlers.AllowCredentials()
+
 
 	/*-----------------------------------------------------------------------
 		User Route:
@@ -39,6 +51,7 @@ func main() {
 		Leave Record Detail		= /leave-record-detail?req_id=1&id=2
 		Leave Record List		= /leave-record-list?id=2&year=ASC
 		Create Leave Record		= /create-leave-record
+		Login					= /login
 	 ------------------------------------------------------------------------*/
 	router.HandleFunc("/user-list", userController.GetUserList).Methods(http.MethodGet)
 	router.HandleFunc("/leave-balance", leaveBalanceController.GetLeaveBalance).Methods(http.MethodGet)
@@ -46,9 +59,11 @@ func main() {
 	router.HandleFunc("/leave-record-list", leaveRecordController.GetLeaveRecordList).Methods(http.MethodGet)
 	router.HandleFunc("/create-leave-record", leaveRecordController.CreateLeaveRecord).Methods(http.MethodPost)
 	router.HandleFunc("/user-detail", userController.GetUserDetail).Methods(http.MethodGet)
-
+	router.HandleFunc("/register", authController.Register).Methods(http.MethodPost)
+	router.HandleFunc("/login", authController.Login).Methods(http.MethodPost)
 	//Start server
 	log.Println("| Server listening on port: 8000")
-	log.Fatal(http.ListenAndServe("localhost:8000", router))
+	
+	log.Fatal(http.ListenAndServe("localhost:8000",handlers.CORS(a,b,c,d,e)(router)))
 
 }
