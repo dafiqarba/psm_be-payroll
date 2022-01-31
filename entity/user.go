39aad39ff5f761adfa0eec684503e7fc684e7375
dafiqarba <daviqarba@live.com>
2022-01-31 18:03:17 +0700

    Modify the order of user entity to correspond table schema

--- entity/user.go
+++ entity/user.go
@@ -3,12 +3,12 @@ package entity
 //User represents users table in the database
 type User struct {
 	User_id     int    `json:"user_id"`
-	Email       string `json:"email"`
-	Password    string `json:"password"`
 	Name        string `json:"name"`
-	Position_id string `json:"position"`
+	Password    string `json:"password"`
+	Email       string `json:"email"`
 	Nik         string `json:"nik"`
-	Role_id     string `json:"role_id"`
+	Role_id     int `json:"role_id"`
+	Position_id int `json:"position"`
 }
 
 type UserDetailModel struct {
@@ -19,4 +19,11 @@ type UserDetailModel struct {
 	Role_id       int    `json:"role_id"`
 	Role_name     string `json:"role"`
 	Position_name string `json:"position"`
+}
+
+type UserLogin struct {
+	User_id  int    `json:"user_id"`
+	Email    string `json:"email"`
+	Password string `json:"-"`
+	Role_id  int    `json:"role_id"`
 }
