8b63e0c910c1236148d2b9b59f954908ce970e44
dafiqarba <daviqarba@live.com>
2022-01-31 18:02:19 +0700

    Modify login/register dto model

--- dto/user-dto.go
+++ dto/user-dto.go
@@ -1,20 +1,17 @@
 package dto
 
-//Response model representation for user detail
-type UserDetailRes struct {
-	User_id     int    `json:"user_id"`
-	Name        string `json:"name"`
-	Position_id string `json:"position"`
-	Nik         string `json:"nik"`
-	Role_id     string `json:"role_id"`
-}
-
-type UserLoginRes struct {
-	User_id  int    `json:"user_id"`
+//Login data model
+type UserLogin struct {
 	Email    string `json:"email"`
 	Password string `json:"password"`
-	Name     string `json:"-"`
-	Position string `json:"-"`
-	Nik      string `json:"-"`
-	Role_id  string `json:"role_id"`
+}
+
+//Register data model
+type RegisterUser struct {
+	Name        string `json:"name"`
+	Password    string `json:"password"`
+	Email       string `json:"email"`
+	Nik         string `json:"nik"`
+	Role_id     int    `json:"role_id"`
+	Position_id int    `json:"position"`
 }
