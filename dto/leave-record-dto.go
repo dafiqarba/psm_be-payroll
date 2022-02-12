89ac2e3825278b6da713e921e328ff18c7465c39
dafiqarba <daviqarba@live.com>
2022-02-12 14:36:02 +0700

    Add amount and change user_id type to int

--- dto/leave-record-dto.go
+++ dto/leave-record-dto.go
@@ -6,10 +6,11 @@ type CreateLeaveRecordModel struct {
 	From_date   string `json:"from_date"`
 	To_date     string `json:"to_date"`
 	Return_date string `json:"return_date"`
+	Amount      string    `json:"amount"`
 	Reason      string `json:"reason"`
 	Mobile      string `json:"mobile"`
 	Address     string `json:"address"`
 	Status_id   string `json:"status_id"`
 	Leave_id    string `json:"leave_id"`
-	User_id     string `json:"user_id"`
+	User_id     int `json:"user_id"`
 }
