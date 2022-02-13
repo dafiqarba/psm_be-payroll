81ceb33fb43279ecd51bd53ea5d8a5b9e6007660
dafiqarba <daviqarba@live.com>
2022-02-13 21:55:18 +0700

    Change data type to int from string for LeaveBalance entity and add UpdateLeaveBalanceModel entity

--- entity/leave-balance.go
+++ entity/leave-balance.go
@@ -3,10 +3,18 @@ package entity
 type LeaveBalance struct {
 	Balance_id   string `json:"balance_id"`
 	Balance_year string `json:"balance_year"`
-	Cuti_tahunan string `json:"cuti_tahunan"`
-	Cuti_diambil string `json:"cuti_diambil"`
-	Cuti_balance string `json:"cuti_balance"`
-	Cuti_izin    string `json:"cuti_izin"`
-	Cuti_sakit   string `json:"cuti_sakit"`
+	Cuti_tahunan int    `json:"cuti_tahunan"`
+	Cuti_diambil int    `json:"cuti_diambil"`
+	Cuti_balance int    `json:"cuti_balance"`
+	Cuti_izin    int    `json:"cuti_izin"`
+	Cuti_sakit   int    `json:"cuti_sakit"`
 	User_id      string `json:"user_id"`
+}
+
+type UpdateLeaveBalanceModel struct {
+	User_id      int    `json:"-"`
+	Year         string `json:"year"`
+	Amounts      int    `json:"amounts"`
+	Leave_id     int    `json:"leave_id"`
+	Cuti_diambil int    `json:"-"`
 }
